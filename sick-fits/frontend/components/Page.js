f7f2013958e10541f842ceadae702dbeeeb9369f
notdrone <notdrone@gmail.com>
2018-09-23 23:04:33 +0530

    styled component button

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,6 +1,22 @@
 import React, { Component } from 'react'
 import Header from './Header'
 import Meta from './Meta'
+import styled from 'styled-components'
+
+const MyButton = styled.button`
+  background-color: red;
+  padding: 4px;
+  font-size: 1rem;
+  color: white;
+  border-radius: 4px;
+  span {
+    font-size: 2rem;
+  }
+`
+
+const BigPoop = styled.span`
+  font-size: 3rem;
+`
 
 class Page extends Component {
   render() {
@@ -8,6 +24,9 @@ class Page extends Component {
       <div>
         <Meta />
         <Header />
+        <MyButton>
+          Sick!!!! <span>💩</span>
+        </MyButton>
         {this.props.children}
       </div>
     )
